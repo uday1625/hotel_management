001906c7a1caa6baf04001b2779b47da487a40ec
uday1625 <161495363+uday1625@users.noreply.github.com>
2026-06-19 17:35:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-# config.py
-HOST = "localhost"
-USER = "root"
-PASSWORD = "Uday@1625"
-DATABASE = "Hotel"
+import os
+
+# Hostname ko .i.aivencloud.com ke sath sahi kar diya hai
+HOST = os.getenv("DB_HOST", "hotel-db-udayprajapati1625-6ae3.i.aivencloud.com")
+USER = os.getenv("DB_USER", "avnadmin")
+PASSWORD = os.getenv("DB_PASSWORD", "") # Aiven dashboard wala password yahan likhein
+DATABASE = os.getenv("DB_NAME", "defaultdb")  
+PORT = int(os.getenv("DB_PORT", 21216))
